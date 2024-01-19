8ad2b233297dcdf49a0bd2cd856dbb6a2e2970ef
Gonzalo Pastor Gómez <147091174+gonxoPG@users.noreply.github.com>
2024-01-19 10:42:45 +0100

    Subida manual

---
+++ Tres en raya/src/TresEnRaya.java
@@ -0,0 +1,24 @@
+public class TresEnRaya {
+
+    static char[][] tablero = new char[3][3];
+    public static void main(String[] args) {
+        System.out.println("Vamos a jugar al tres en raya");
+
+        char J1 = 'X';
+        char J2 = 'O';
+        boolean raya = false;
+
+    }
+
+    public static comprobarRaya(){
+        for (int i = 0; i < tablero.length; i++) {
+            for (int j = 0; j < tablero[i].length; j++) {
+                if (tablero[0][j]=='X' && tablero[0][j]=='O'){
+
+                } else if (tablero[1][j]=='X' && tablero[1][j]=='O') {
+
+                }else
+            }
+        }
+    }
+}
